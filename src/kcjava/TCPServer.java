0667be75e16244c2d7919d1cfa3b85817d788d4e
LehyanFlouriot <57806792+LehyanFlouriot@users.noreply.github.com>
2021-05-21 11:22:44 +0200

    Update TCPServer.java

--- src/kcjava/TCPServer.java
+++ src/kcjava/TCPServer.java
@@ -1,5 +1,6 @@
 package kcjava;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
@@ -7,31 +8,67 @@ import java.net.Socket;
 
 
 public class TCPServer{
-	 public static final int PORT = 3191;
+	public static final int PORT = 3191;
 
 	public static void main(String[] args) throws Exception{
 		new TCPServer();
 	}
 	public TCPServer() throws Exception{
-			ServerSocket socketserver = new ServerSocket(PORT);
+			ServerSocket serverSocket = null;
 			
-			System.out.println("Le serveur est à l'écoute du port "+ PORT);
-			Socket socket = socketserver.accept();
+			// Nombre de connections
+			int clientNumber = 0;
 			
-			System.out.println("Connexion client acceptée.");
+			// Essaye d'ouvrir le serveur socket sur le port PORT
+			try {
+				serverSocket = new ServerSocket(PORT);
+				System.out.println("Le serveur est à l'écoute du port "+ PORT);
+			}catch(IOException e) {
+				System.out.println(e);
+			}
+			try {
+				while(true) {
+					Socket socket = serverSocket.accept();
+					System.out.println("Connexion client acceptée.");
+					new ServiceThread(socket, clientNumber++).start();
+				}
+			}finally {
+				serverSocket.close();
+			}
+	}
+	private class ServiceThread extends Thread{
+		private int clientNumber;
+		private Socket socket;
+		
+		public ServiceThread(Socket socket, int clientNumber) {
+			this.clientNumber = clientNumber;
+			this.socket = socket;
 			
-			ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-			ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-			
- 
-			
-			CheckTime check = (CheckTime)ois.readObject();
-			
-			System.out.println(check.getIdentifiant());
-			
-			CheckTime reponse = new CheckTime(17);
-			oos.writeObject(reponse);
-			
-			socketserver.close();
+			System.out.println("New connection with " + this.clientNumber + "at" + socket);
+		}
+		@Override
+		public void run() {
+			try {
+				ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+				ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+				
+				while(true) {
+					// On recup le checkTime envoye par la pointeuse
+					CheckTime check = (CheckTime)inputStream.readObject();
+					System.out.println("Donnees recup sur employe nb : "+check.getIdentifiant());
+					
+					
+					// On repond a la poiteuse
+					String rep = new String("Bien recu");
+					outputStream.writeObject(rep);
+				}
+				
+			}catch(IOException e) {
+				System.out.println(e);
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
